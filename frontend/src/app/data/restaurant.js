3fe885f04d9c6f27a7479d885184006d3bb6a39f
yanirc1981 <cerzurc@hotmail.com>
2024-07-10 18:35:42 -0300

    Se agregan arrays de data

---
+++ frontend/src/app/data/restaurant.js
@@ -0,0 +1,65 @@
+const restaurantes = [
+    {
+        nombre: "Restaurante El Samán",
+        ubicacion: "Calle 31 #30-45, Guatapé, Antioquia, Colombia",
+        estrellas: 4.6,
+        imagen: "https://example.com/restaurante-el-saman.jpg"
+      },
+      {
+        nombre: "La Fogata",
+        ubicacion: "Calle 32 #27-20, Guatapé, Antioquia, Colombia",
+        estrellas: 4.5,
+        imagen: "https://example.com/la-fogata.jpg"
+      },
+      {
+        nombre: "Pizzeria De Luigi",
+        ubicacion: "Calle 30 #28-28, Guatapé, Antioquia, Colombia",
+        estrellas: 4.7,
+        imagen: "https://example.com/pizzeria-de-luigi.jpg"
+      },
+      {
+        nombre: "Thai Terrace",
+        ubicacion: "Carrera 30 #31-45, Guatapé, Antioquia, Colombia",
+        estrellas: 4.8,
+        imagen: "https://example.com/thai-terrace.jpg"
+      },
+      {
+        nombre: "Restaurante Mi Casa",
+        ubicacion: "Carrera 28 #30-15, Guatapé, Antioquia, Colombia",
+        estrellas: 4.4,
+        imagen: "https://example.com/restaurante-mi-casa.jpg"
+      },
+      {
+        nombre: "La Mona",
+        ubicacion: "Calle 32 #27-12, Guatapé, Antioquia, Colombia",
+        estrellas: 4.3,
+        imagen: "https://example.com/la-mona.jpg"
+      },
+      {
+        nombre: "Cafe La Vina",
+        ubicacion: "Calle 31 #30-21, Guatapé, Antioquia, Colombia",
+        estrellas: 4.6,
+        imagen: "https://example.com/cafe-la-vina.jpg"
+      },
+      {
+        nombre: "Don De Sam",
+        ubicacion: "Calle 32 #29-47, Guatapé, Antioquia, Colombia",
+        estrellas: 4.7,
+        imagen: "https://example.com/don-de-sam.jpg"
+      },
+      {
+        nombre: "Restaurante Mediterráneo",
+        ubicacion: "Calle 31 #28-12, Guatapé, Antioquia, Colombia",
+        estrellas: 4.5,
+        imagen: "https://example.com/restaurante-mediterraneo.jpg"
+      },
+      {
+        nombre: "Restaurante El Patio",
+        ubicacion: "Calle 31 #30-38, Guatapé, Antioquia, Colombia",
+        estrellas: 4.6,
+        imagen: "https://example.com/restaurante-el-patio.jpg"
+      }
+  ];
+  
+  export default restaurantes;
+  
