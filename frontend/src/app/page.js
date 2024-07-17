2105b09be748b507573257309cdadc8e368f35d7
matias-saig <saig.matias@gmail.com>
2024-07-17 20:35:22 -0300

    fuentes y colores

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,22 +1,44 @@
-import React from 'react';
-import Navbar from '@/components/navbar/page';
-import Banner from '@/components/banner/page';
-import Filters from '@/components/filters/page';
-import Cards from '../components/cards/cards';
-import Footer from '@/components/footer/page';
-
+import React from "react";
+import Navbar from "@/components/navbar/page";
+import Banner from "@/components/banner/page";
+import Filters from "@/components/filters/page";
+import Cards from "../components/cards/cards";
+import Footer from "@/components/footer/page";
 
 export default function Home() {
   return (
     <div>
- 
       <main>
-        <Navbar/>
-        <Banner/>
-        <Filters/>
-        <Cards/>
-        <Footer/>
-      
+        <Navbar />
+        <Banner />
+
+        {/* Elementos de muestra */}
+        <div className="m-40">
+          <h2 className="text-center text-lg">Colores</h2>
+          <p className="bg-customGrayLight"> customGrayLight</p>
+          <p className="bg-customGray"> customGray</p>
+          <p className="bg-customGreenLight"> customGreenLight</p>
+
+          <p className="text-white bg-customGreen"> customGreen</p>
+          <p className="text-white bg-customBlue"> customBlue</p>
+          <p className="text-white bg-customGrayDark"> customGrayDark</p>
+          <p className="text-white bg-customBrown"> customBrown</p>
+          <p className="text-white bg-customBlueDark"> customBlueDark</p>
+        </div>
+
+        <div className="m-40">
+          <h2 className="text-center text-lg">Fuentes</h2>
+          <p className="m-5 text-lg font-txBarlow">Barlow</p>
+          <p className="m-5 text-lg font-txPoppins">Poppins</p>
+          <p className="m-5 text-lg font-txTitle">Titulo cavean Brush</p>
+          <p className="m-5 text-lg font-txMogra">Mogra</p>
+          <p className="m-5 text-lg font-txSpartan">League Spartan</p>
+          <p className="m-5 text-lg font-txSansita">Sansita</p>
+        </div>
+
+        <Filters />
+        <Cards />
+        <Footer />
       </main>
     </div>
   );
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -12,6 +12,29 @@ module.exports = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        customGrayLight: "#FBFBFB",
+        customGray: "#F2F2F2",
+        customGreenLight: "#F5FFF8",
+        customGreen: "#3B764C",
+        customBrown: "#363230",
+        customGrayDark: "#2E2E2E",
+        customBlue: "#5289F3",
+        customBlueDark: "#202937",
+      },
+      fontFamily: {
+        txBarlow: ['Barlow', 'sans-serif'],
+        txPoppins: ['Poppins', 'sans-serif'],
+        txTitle: ['Caveat Brush', 'sans-serif'],
+        txSpartan: ['League Spartan', 'sans-serif'],
+        txMogra: ['Mogra', 'sans-serif'],
+        txSansita: ['Sansita', 'serif'],
+
+
+
+
+      },
+
     },
   },
   plugins: [],
