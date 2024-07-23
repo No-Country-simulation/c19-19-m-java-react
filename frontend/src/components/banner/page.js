39fb4d4b9f81f9447a36f5101f926beeb1b01288
Juan Diego Montenegro Segura <menethil0503@gmail.com>
2024-07-23 11:02:48 -0500

    Diseño del banner, removiendo filter del page principal y agregarlo en el banner

--- frontend/src/components/banner/page.js
+++ frontend/src/components/banner/page.js
@@ -2,12 +2,13 @@ import Title from '../texts/Title';
 import Image from 'next/image';
 import Timeset from './Timeset';
 import Wheater from './Wheater';
+import Filters from '../filters/page';
 
 const Banner = () => {
 
   return (
     <header className="bg-cover bg-center">
-      <div className="bg-black/30 relative w-full h-[50rem] flex items-center justify-center">
+      <div className="bg-black/30 relative w-full h-[55rem] flex flex-col items-center justify-center">
 
         {/* Video de Fondo */}
         <video className="w-full h-full object-cover absolute -z-10" loop autoPlay muted>
@@ -15,7 +16,7 @@ const Banner = () => {
         </video>
 
         {/* Tiempo y Clima */}
-        <div className="absolute top-[43rem] left-0 p-4 flex flex-col items-start text-white animate-fade-right animate-delay-[700ms] animate-ease-in-out">
+        <div className="absolute top-[31rem] min-[425px]:top-[33rem] sm:top-[35rem] left-0 p-4 flex flex-col items-start text-white animate-fade-right animate-delay-[700ms] animate-ease-in-out">
           <span className="flex items-center gap-2 mb-2">
             <Image src={"/time.svg"} width={25} height={25} alt="Time Icons" />
             <Timeset>Guatapé, Colombia</Timeset>
@@ -28,9 +29,14 @@ const Banner = () => {
         </div>
 
         {/* Título */}
-        <div className="text-center px-4 lg:pl-8 text-white">
-          <Title styleAdd=" text-8xl animate-fade-up animate-ease-in-out">Rincones de Guatapé</Title>
-          <Title styleAdd=" text-5xl animate-fade-up animate-delay-200 animate-ease-in-out">Un paraíso escondido en Colombia</Title>
+        <div className="text-center px-4 text-white my-auto pt-28">
+          <Title styleAdd=" text-7xl sm:text-8xl animate-fade-up animate-ease-in-out">Rincones de Guatapé</Title>
+          <Title styleAdd=" text-4xl sm:text-5xl animate-fade-up animate-delay-200 animate-ease-in-out">Un paraíso escondido en Colombia</Title>
+        </div>
+
+        {/* Filtros */}
+        <div className=' mb-8 px-4 w-full'>
+          <Filters />
         </div>
 
       </div>
