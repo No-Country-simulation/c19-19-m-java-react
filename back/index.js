e23f267f83222fd3f7c6dc25122dc88dec0ce55e
yanirc1981 <cerzurc@hotmail.com>
2024-07-24 23:08:05 -0300

    se agrega helpers

--- back/index.js
+++ back/index.js
@@ -2,9 +2,15 @@ const { conn } = require('./src/data');
 const { PORT } = require('./src/config/envs.js');
 require('dotenv').config();
 const app = require('./src/app.js')
+const loadPlaces = require('./src/helpers/loadData.js')
 
 // Syncing all the models at once.
 conn.sync({ alter: true }).then(async () => {
+
+  await loadPlaces()
+
+
+  
   app.listen(PORT, () => {
     console.log(`🚀 listening on port: ${PORT} 🚀`);
   });
--- back/src/dataMokeada/actividades.js
+++ back/src/dataMokeada/actividades.js
@@ -1,72 +1,92 @@
 const actividades = [
     {
-        nombre: "Subida a la Piedra del Peñol",
-        ubicacion: "La Piedra del Peñol, Guatapé, Antioquia, Colombia",
-        tipo: "Diurna",
-        estrellas: 4.9,
+      nombre: "Subida a la Piedra del Peñol",
+      ubicacion:"Guatapé, Antioquia, Colombia",
+      tipo: 'actividad',
+      estrellas: 4.9,
+      descripcion: "Una emocionante subida a la icónica Piedra del Peñol.",
+      n_document: "30772620",
         imagen: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcThPkt6kqxGcXptwUoQk6NRIvdVZ7fEYZ7x9Q&s"
       },
       {
         nombre: "Tour en lancha por la represa",
-        ubicacion: "Embalse Peñol-Guatapé, Guatapé, Antioquia, Colombia",
-        tipo: "Diurna",
+        ubicacion: " Guatapé, Antioquia, Colombia",
+        tipo: 'actividad',
         estrellas: 4.8,
+        descripcion: "Un tour en lancha por la hermosa represa.",
+        n_document: "30772620",
         imagen: "https://imagescdn.citix.com.co/citix/production/media/media/eb1efd0a7750fd54b86e3dd7908ff6ef.jpg"
       },
       {
         nombre: "Paseo en bicicleta por el malecón",
-        ubicacion: "Malecón de Guatapé, Guatapé, Antioquia, Colombia",
-        tipo: "Diurna",
-        estrellas: 4.7,
+    ubicacion: "Guatapé, Antioquia, Colombia",
+    tipo: 'actividad',
+    estrellas: 4.7,
+    descripcion: "Un paseo relajante en bicicleta por el malecón.",
+    n_document: "30772620",
         imagen: "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/25/ae/fd/ed/caption.jpg?w=500&h=400&s=1"
       },
       {
         nombre: "Noche de karaoke en La Fogata",
-        ubicacion: "Calle 32 #27-20, Guatapé, Antioquia, Colombia",
-        tipo: "Nocturna",
-        estrellas: 4.6,
+    ubicacion: "Guatapé, Antioquia, Colombia",
+    tipo: 'actividad',
+    estrellas: 4.6,
+    descripcion: "Diviértete con una noche de karaoke.",
+    n_document: "30772620",
         imagen: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR0IIputAeZwHyvL_YiF_4_5gMjjESYYPSB1Q&s"
       },
       {
         nombre: "Caminata ecológica por el Parque Principal",
-        ubicacion: "Parque Principal, Guatapé, Antioquia, Colombia",
-        tipo: "Diurna",
-        estrellas: 4.5,
+    ubicacion: "Guatapé, Antioquia, Colombia",
+    tipo: 'actividad',
+    estrellas: 4.5,
+    descripcion: "Una caminata ecológica en el parque.",
+    n_document: "30772620",
         imagen: "https://ecoglobalexpeditions.com/wp-content/uploads/2017/04/La-Ceja-de-Guatape-2-1.jpg"
       },
       {
         nombre: "Cena con música en vivo en Thai Terrace",
-        ubicacion: "Carrera 30 #31-45, Guatapé, Antioquia, Colombia",
-        tipo: "Nocturna",
+        ubicacion: "Guatapé, Antioquia, Colombia",
+        tipo: 'actividad',
         estrellas: 4.8,
+        descripcion: "Disfruta de una cena con música en vivo.",
+        n_document: "30772620",
         imagen: "https://files.lamega.com.rcnra-dev.com/assets/public/styles/d_img_1200x630/public/media/image/image/2022-10/Paola%20Jara%20%282%29.png?h=89c062dc&itok=MCotuu0C"
       },
       {
         nombre: "Visita al Museo Histórico",
-        ubicacion: "Calle 31 #28-60, Guatapé, Antioquia, Colombia",
-        tipo: "Diurna",
-        estrellas: 4.4,
+    ubicacion: "Guatapé, Antioquia, Colombia",
+    tipo: 'actividad',
+    estrellas: 4.4,
+    descripcion: "Visita al museo histórico de Guatapé.",
+    n_document: "30772620",
         imagen: "https://viajaporcolombia.com/images/guatape-pueblo-de-zocalos-1.jpg"
       },
       {
         nombre: "Noche de cine en el Cine Guatapé",
-        ubicacion: "Calle 32 #30-45, Guatapé, Antioquia, Colombia",
-        tipo: "Nocturna",
+        ubicacion: "Guatapé, Antioquia, Colombia",
+        tipo: 'actividad',
         estrellas: 4.3,
+        descripcion: "Una noche de cine en el Cine Guatapé.",
+        n_document: "30772620",
         imagen: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcThO4grVyT1Fp6cmoEz-pN4CcFgMgpBqUG3BA&s"
       },
       {
         nombre: "Pesca en la represa",
-        ubicacion: "Embalse Peñol-Guatapé, Guatapé, Antioquia, Colombia",
-        tipo: "Diurna",
-        estrellas: 4.7,
+    ubicacion: "Guatapé, Antioquia, Colombia",
+    tipo: 'actividad',
+    estrellas: 4.7,
+    descripcion: "Pesca en la represa de Peñol-Guatapé.",
+    n_document: "30772620",
         imagen: "https://www.civitatis.com/f/colombia/guatape/pesca-embalse-guatape-589x392.jpg"
       },
       {
         nombre: "Tour gastronómico por el centro",
-        ubicacion: "Centro de Guatapé, Guatapé, Antioquia, Colombia",
-        tipo: "Nocturna",
-        estrellas: 4.6,
+    ubicacion: "Guatapé, Antioquia, Colombia",
+    tipo: 'actividad',
+    estrellas: 4.6,
+    descripcion: "Tour gastronómico por el centro de Guatapé.",
+    n_document: "30772620",
         imagen: "https://where.com.co/wp-content/uploads/EXPERIENCIA-Where-AEROTUREX-tour-guatape-gastronomico-POST-01-1-copia.webp"
       }
   ];
