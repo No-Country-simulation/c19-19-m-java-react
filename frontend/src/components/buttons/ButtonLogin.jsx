c489c5bf3ff27ea9dd53b4af10ebd4b50aeba02f
Matias-Saig <saig.matias@gmail.com>
2024-07-28 18:58:32 -0300

    Add button superForm

--- frontend/src/components/buttons/ButtonLogin.jsx
+++ frontend/src/components/buttons/ButtonLogin.jsx
@@ -10,75 +10,84 @@
         <ButtonLogin to={"a"} logout={true} />
         <ButtonLogin to={"a"} addUser={true} />
         <ButtonLogin to={"a"} superAdmin={true} />
+        <ButtonLogin to={"a"} superForm={true} />
+
 
 
  */
 
-        import {
-          AiOutlineEdit,
-          AiOutlineLogout,
-          AiOutlineTool,
-          AiOutlineUser,
-          AiOutlineUserAdd,
-        } from "react-icons/ai";
-        import ButtonSession from "./ButtonSession";
-        
-        export default function ButtonLogin({
-          User = false,
-          nouser = false,
-          addUser = false,
-          logout = false,
-          Admin = false,
-          SuperAdmin = false,
-          edit = false,
-          onClick,
-          to,
-        }) {
-          const icon = "fill-customGreenLight size-6";
-          
-          return (
-            <>
-              {nouser && (
-                <ButtonSession to={to} addStyle={"bg-gray-400"}>
-                  <AiOutlineUser className={icon} />
-                </ButtonSession>
-              )}
-              {User && (
-                <ButtonSession to={to} addStyle={"bg-customGreen"}>
-                  <AiOutlineUser className={icon} />
-                </ButtonSession>
-              )}
-              {addUser && (
-                <ButtonSession to={to} addStyle={"bg-customGrayDark"}>
-                  <AiOutlineUserAdd className={icon} />
-                </ButtonSession>
-              )}
-              {logout && (
-                <ButtonSession to={to} addStyle={"bg-customGrayDark"} onClick={onClick}>
-                  <AiOutlineLogout className={icon} />
-                </ButtonSession>
-              )}
-              {Admin && (
-                <ButtonSession to={to} addStyle={"bg-customBlue"}>
-                  <AiOutlineUser className={icon} />
-                </ButtonSession>
-              )}
-              {SuperAdmin && (
-                <ButtonSession to={to} addStyle={"bg-customBlue"}>
-                  <AiOutlineUser className={icon} />
-                </ButtonSession>
-              )}
-              {edit && (
-                <ButtonSession to={to} addStyle={"bg-customBlue"}>
-                  <AiOutlineEdit className={icon} />
-                </ButtonSession>
-              )}
-              {SuperAdmin && (
-                <ButtonSession to={to} addStyle={"bg-customBlue"}>
-                  <AiOutlineTool className={icon} />
-                </ButtonSession>
-              )}
-              
-            </>
-          );
-        }
+import {
+  AiOutlineEdit,
+  AiOutlineForm,
+  AiOutlineLogout,
+  AiOutlineTool,
+  AiOutlineUser,
+  AiOutlineUserAdd,
+} from "react-icons/ai";
+import ButtonSession from "./ButtonSession";
+
+export default function ButtonLogin({
+  User = false,
+  nouser = false,
+  addUser = false,
+  logout = false,
+  Admin = false,
+  SuperAdmin = false,
+  SuperForm = false,
+  edit = false,
+  onClick,
+  to,
+}) {
+  const icon = "fill-customGreenLight size-6";
+
+  return (
+    <>
+      {nouser && (
+        <ButtonSession to={to} addStyle={"bg-gray-400"}>
+          <AiOutlineUser className={icon} />
+        </ButtonSession>
+      )}
+      {User && (
+        <ButtonSession to={to} addStyle={"bg-customGreen"}>
+          <AiOutlineUser className={icon} />
+        </ButtonSession>
+      )}
+      {addUser && (
+        <ButtonSession to={to} addStyle={"bg-customGrayDark"}>
+          <AiOutlineUserAdd className={icon} />
+        </ButtonSession>
+      )}
+      {logout && (
+        <ButtonSession to={to} addStyle={"bg-customGrayDark"} onClick={onClick}>
+          <AiOutlineLogout className={icon} />
+        </ButtonSession>
+      )}
+      {Admin && (
+        <ButtonSession to={to} addStyle={"bg-customBlue"}>
+          <AiOutlineUser className={icon} />
+        </ButtonSession>
+      )}
+      {SuperAdmin && (
+        <ButtonSession to={to} addStyle={"bg-customBlue"}>
+          <AiOutlineUser className={icon} />
+        </ButtonSession>
+      )}
+      {edit && (
+        <ButtonSession to={to} addStyle={"bg-customBlue"}>
+          <AiOutlineEdit className={icon} />
+        </ButtonSession>
+      )}
+      {SuperAdmin && (
+        <ButtonSession to={to} addStyle={"bg-customBlue"}>
+          <AiOutlineTool className={icon} />
+        </ButtonSession>
+      )}
+
+      {SuperForm && (
+        <ButtonSession to={to} addStyle={"bg-customBlue"}>
+          <AiOutlineForm className={icon} />
+        </ButtonSession>
+      )}
+    </>
+  );
+}
