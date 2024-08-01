89b4619493a349c7a5d697b05651e87d0acf8641
Juan Diego Montenegro Segura <menethil0503@gmail.com>
2024-08-01 07:33:13 -0500

    Arreglando problemas de las imagenes

--- frontend/src/app/detalle/[id]/page.jsx
+++ frontend/src/app/detalle/[id]/page.jsx
@@ -156,7 +156,7 @@ const Detalle = ({ params }) => {
           <h2 className="text-5xl font-bold tracking-tight text-gray-900 mb-4">{place.nombre}</h2>
           <p className="text-lg text-black mb-4 flex items-center gap-1"><img src="/location.svg" alt="" className=' size-5'/> Ubicación: {place.ubicacion} Guatapé</p>
         </div>
-        <div className=" grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 grid-flow-dense gap-4 w-full h-[20rem]">
+        <div className=" grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 grid-flow-dense gap-4 w-full h-auto">
           {place.Images && place.Images.length > 0 ? (
             place.Images.map((image) => (
               <div
